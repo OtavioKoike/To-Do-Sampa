4c7ff8dc1806b318282fb7bb081158c3fa681115
OtavioKoike <otaviokoike07@gmail.com>
2021-06-04 15:29:56 -0300

    Project Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+// Import routing module
+import { AppRoutingModule } from './app.routing';
+
+// Angular Material
+import {MatButtonModule} from '@angular/material/button';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
