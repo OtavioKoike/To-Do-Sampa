71ebd3a16e1ecff936c427d6f0fdc1778ad4f496
OtavioKoike <otaviokoike07@gmail.com>
2021-06-04 20:56:21 -0300

    Finalizado Sistema de Login

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 //Guards
 import { AuthGuard } from './guard/auth.guard';
+import { SecureInnerPagesGuard } from './guard/secure-inner-pages.guard';
+
 //Login
 import { VerifyEmailComponent } from './screens/Login/verify-email/verify-email.component';
 import { ForgotPasswordComponent } from './screens/Login/forgot-password/forgot-password.component';
@@ -13,10 +15,10 @@ import { HomeComponent } from './screens/home/home.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
-  { path: 'sign-in', component: SignInComponent },
-  { path: 'register-user', component: SignUpComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
-  { path: 'verify-email-address', component: VerifyEmailComponent },
+  { path: 'sign-in', component: SignInComponent, canActivate: [SecureInnerPagesGuard]},
+  { path: 'register-user', component: SignUpComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'forgot-password', component: ForgotPasswordComponent, canActivate: [SecureInnerPagesGuard] },
+  { path: 'verify-email-address', component: VerifyEmailComponent, canActivate: [SecureInnerPagesGuard] },
 
   {path: 'home', component: HomeComponent, canActivate: [AuthGuard]}
 ]
